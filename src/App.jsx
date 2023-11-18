9f5b3c4c3d595b7cb5342dbdbeab47fa6773d594
Vikas Bhalla <vikas@ellingsentechnologies.com>
2023-11-18 16:38:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer} from "./sections/index"
+
+const App = () => (
+  <main className='relative'>
+    Nav
+    <section className='xl:padding-1 wide:padding-r padding-b'>
+      <Hero />
+    </section>
+    <section className='padding'>
+      <PopularProducts />
+    </section>
+    <section className='padding'>
+      <SuperQuality />
+    </section>
+    <section className='padding-x py-10'>
+      <Services/>
+    </section>
+    <section className='padding'>
+      <SpecialOffer />
+    </section>
+    <section className='padding bg-pale-blue'>
+      <CustomerReviews />
+    </section>
+    <section className='padding-x sp:py-32 py-6 w-full'>
+      <Subscribe />
+    </section>
+    <section className='padding-x bg-black padding-t pb-8'>
+      <Footer />
+    </section>
+
+  </main>
+)
+
+export default App
