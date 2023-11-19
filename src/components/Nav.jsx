37cb07579892b8f82fbd01630f8e6927dbbe9ef5
Vikas Bhalla <vikas@ellingsentechnologies.com>
2023-11-19 15:44:35 +0530

    Basic Nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {headerLogo} from "../assets/images"
+import { navLinks } from '../constants'
+import { hamburger } from '../assets/icons'
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
+            <a href="/">
+                <img 
+                    src={headerLogo}
+                    alt='Logo'
+                    width={130}
+                    height={29}
+                />
+            </a>
+
+            <ul className='flex justify-center items-center gap-16 max-lg:hidden'>
+              {
+                navLinks.map((item)=> (
+                  <li key={item.label}>
+                    <a href={item.href} className='font-montserrat leading-normal text-slate-gray'>{item.label}</a>
+                  </li>
+                ))
+              }
+            </ul>
+
+            <div className='hidden max-lg:block'>
+              <img 
+                src={hamburger} 
+                alt='Menu'
+                width={25}
+                height={25}
+              />
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
