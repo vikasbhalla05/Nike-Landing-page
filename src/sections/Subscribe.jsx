60eab96ef8b711b20999718164e0c966e42fcf26
Vikas Bhalla <vikas@ellingsentechnologies.com>
2023-11-25 13:04:47 +0530

    feature - subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Button from "../components/Button"
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className='max-container my-10 flex justify-between gap-10 items-center max-lg:flex-col' id='contact-us'>
+      <h3 className='text-center font-palanquin text-4xl font-bold'>
+        Sign Up for <span className='text-coral-red'>Updates</span> & Newsletter
+      </h3>
+
+      <div className='flex border-solid border-2 border-slate-gray p-2 rounded-full max-lg:w-[80%] w-[40%]'>
+        <input type='email'
+          placeholder='subscribe@nike.com'
+          className='input bg-transparent'
+        />
+        <div>
+          <Button label="Sign Up" type="red"/>
+        </div>
+      </div>
+    </section>
   )
 }
 
