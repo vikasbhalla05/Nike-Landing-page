288ce157d56b99a897eff765dffeef2423705625
Vikas Bhalla <vikas@ellingsentechnologies.com>
2023-11-26 23:12:16 +0530

    fix - service card fixes

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ServiceCard = ({imgURL,label,subtext}) => {
     console.log(imgURL)
   return (
-    <div className='rounded-md shadow-3xl ring-1 ring-gray-100 shadow-gray-200 px-10 py-16 w-[80%] sm:w-[350px] sm:min-w-[350px]'>
+    <div className='rounded-md shadow-3xl ring-1 ring-gray-100 shadow-gray-200 px-10 py-16 w-full sm:max-w-md'>
         <div className='bg-coral-red p-2 rounded-full w-11 h-11'>
             <img src={imgURL} alt={label} className='w-full h-auto'/>
         </div>
